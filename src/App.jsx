afa75414e7ad4e0e3e412577e4feade0e4020711
pmocz <philip.mocz@gmail.com>
2024-07-29 10:22:11 -0400

    updating app

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default () => (
   <>
-    <h1>Welcome to React Vite Micro App!</h1>
+    <h1>Test Problem Dashboard for Minisim!</h1>
     <p>Hard to get more minimal than this React app.</p>
   </>
 );
