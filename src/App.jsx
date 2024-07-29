a9770c11fe7823dccf5eaf96c7269ab65300449f
pmocz <philip.mocz@gmail.com>
2024-07-29 10:35:31 -0400

    wording

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,6 @@ import React from "react";
 export default () => (
   <>
     <h1>Test Problem Dashboard for Minisim!</h1>
-    <p>Hard to get more minimal than this React app.</p>
+    <p>Add React app here.</p>
   </>
 );
