2b94c2137c736bbeca0612b24130907e431fa435
pmocz <philip.mocz@gmail.com>
2024-09-04 15:43:53 -0400

    adding plotly example

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
 
+import Plot from 'react-plotly.js';
+//import React from 'react';
+
+var my_data = [
+  {
+    x: [1, 2, 3],
+    y: [2, 6, 3],
+    type: 'scatter',
+    mode: 'lines+markers',
+    marker: {color: 'red'},
+  },
+  {type: 'bar', x: [1, 2, 3], y: [2, 5, 3]},
+]
+
+var my_layout = { width: 320, height: 240, title: 'A Fancy Plot' }
+
+function TPPlot({ data, layout }) {
+  return (
+    <Plot data={data} layout={layout} />
+  );
+}
+
 function App() {
   return (
     <div className="App">
+      {/*
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        {/*
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -18,6 +42,8 @@ function App() {
           Learn React
         </a>
       </header>
+      */}
+      <TPPlot data={my_data} layout={my_layout} />
     </div>
   );
 }
